6a56813dbe4f8b2845f5d8932758df6658709d1f
Julio Wandekoken <jwandekoken@gmail.com>
2021-12-31 06:28:28 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="tcp")
+    
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(str(packet[scapy.Raw].load))
+
+
+sniff("eth0")
